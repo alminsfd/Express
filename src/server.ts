d08ebeaa79cf84071004cdcb29613fa83ee4c129
alminsfd <alaminhossaintanvir42@gmail.com>
2026-05-18 14:36:48 +0600

    send the data using req and res of express

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, { type Application, type Request, type Response } from "express"
+const app: Application = express()
+const port = 5000
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+     res.status(200).json({
+
+          message: "Hello developers",
+          "authentication": "NextLeveldeveloper"
+     })
+})
+
+app.post('/', (req: Request, res: Response) => {
+     console.log(req.body);
+})
+
+app.listen(port, () => {
+     console.log(`Example app listening on port ${port}`)
+})
