a17e14d0dbc9c7e66f5481222ca5824ad11a07fd
alminsfd <alaminhossaintanvir42@gmail.com>
2026-05-19 14:39:08 +0600

    Schema  set up done in postgraSQL

---
+++ src/db/index.ts
@@ -0,0 +1,29 @@
+import { Pool } from "pg";
+import config from "../config";
+
+export const pool = new Pool({
+     connectionString: config.connectionString
+})
+
+
+
+export const initDB = async () => {
+     try {
+          await pool.query(`
+               CREATE TABLE IF NOT EXISTS users (
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(50), -- Name er length barano hoyeche
+                    email VARCHAR(100) UNIQUE NOT NULL, -- Email er length barano hoyeche
+                    password VARCHAR(255) NOT NULL, -- Password er length oboshshoi besi hote hobe
+                    is_active BOOLEAN DEFAULT true,
+                    age INT,
+                    created_at TIMESTAMP DEFAULT NOW(), -- Typo thik kora hoyeche (creat_at -> created_at)
+                    updated_at TIMESTAMP DEFAULT NOW()  -- Typo thik kora hoyeche (update_at -> updated_at)
+               )
+          `);
+          console.log("Database table successfully checked/created");
+     } catch (err) {
+          console.error("Database initialization error:", err);
+     }
+}
+
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,13 @@
 import express, { type Application, type Request, type Response } from "express"
 import { Pool } from 'pg'
 import config from "./config"
+import { initDB, pool } from "./db"
 const app: Application = express()
 const port = config.port
 app.use(express.json())
-const pool = new Pool({
-     connectionString: config.connectionString
-})
 // app.use(express.text())
 // app.use(express.urlencoded({ extended: true }))
 
-const initDB = async () => {
-     try {
-          await pool.query(`
-               CREATE TABLE IF NOT EXISTS users (
-                    id SERIAL PRIMARY KEY,
-                    name VARCHAR(50), -- Name er length barano hoyeche
-                    email VARCHAR(100) UNIQUE NOT NULL, -- Email er length barano hoyeche
-                    password VARCHAR(255) NOT NULL, -- Password er length oboshshoi besi hote hobe
-                    is_active BOOLEAN DEFAULT true,
-                    age INT,
-                    created_at TIMESTAMP DEFAULT NOW(), -- Typo thik kora hoyeche (creat_at -> created_at)
-                    updated_at TIMESTAMP DEFAULT NOW()  -- Typo thik kora hoyeche (update_at -> updated_at)
-               )
-          `);
-          console.log("Database table successfully checked/created");
-     } catch (err) {
-          console.error("Database initialization error:", err);
-     }
-}
 
 initDB();
 
